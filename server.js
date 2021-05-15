237074c919afdf4bcbd82ae0f2f6af2b6b2f6a33
Jordi de Pablos <jordi.depablos@bicworld.com>
2021-05-15 21:20:18 +0200

    First version

--- server.js
+++ server.js
@@ -3,12 +3,27 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+const privateKey = fs.existsSync('certs/privkey.pem')
+                      ? fs.readFileSync('certs/privkey.pem', 'utf8')
+                      : undefined;
+const certificate = fs.existsSync('certs/cert.pem')
+                      ? fs.readFileSync('certs/cert.pem', 'utf8')
+                      : undefined;
+const credentials = privateKey !== undefined && certificate !== undefined
+                      ? { key: privateKey, cert: certificate }
+                      : undefined;
+
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -25,9 +40,27 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get('/api/whoami', (req, res, next) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
+});
 
 // listen for requests :)
+let apiServer;
+if (credentials)
+  apiServer = https.createServer(credentials, app);
+else
+  apiServer = http.createServer(app);
+
+apiServer.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + apiServer.address().port);
+});
+
+/*
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+*/
